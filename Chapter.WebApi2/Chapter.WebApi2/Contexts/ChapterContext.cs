add80413adce29496b64481f75955daeef73d03c
MacielDouglas <douglasmaciel.dm@gmail.com>
2022-05-31 19:53:03 -0300

    adicionado o recurso get no Chapter

---
+++ Chapter.WebApi2/Chapter.WebApi2/Contexts/ChapterContext.cs
@@ -0,0 +1,28 @@
+﻿using Chapter.WebApi2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Chapter.WebApi2.Contexts
+{
+    public class ChapterContext : DbContext
+    {
+        public ChapterContext()
+        {
+        }
+        public ChapterContext(DbContextOptions<ChapterContext> options) : base(options)
+        {
+        }
+        // vamos utilizar esse médoto para configurar o bando de dados
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                // Cada provedor tem sua sintaxe para especificação
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-3A2A2GA\\SQLEXPRESS; initial catalog=Chapter; Integrated Security=true");
+            }
+        }
+        // dbset representa as entidades que serão utilizadas nas operações de leitura, criação, atualização e deleção
+        public DbSet<Livro> Livros { get; set; } //Essa classe livro está referenciando a planilha livros no banco de dados.(link da api c/ banco de dados)
+    }
+
+}
---
+++ Chapter.WebApi2/Chapter.WebApi2/Repositories/LivroRepository.cs
@@ -0,0 +1,19 @@
+﻿using Chapter.WebApi2.Contexts;
+using Chapter.WebApi2.Models;
+
+namespace Chapter.WebApi2.Repositories
+{
+    public class LivroRepository // O Repositorio depende da conexão c/ banco para funcionar, isso é Injeção de dependencia.
+    {
+        private readonly ChapterContext _context; //variavel apenas de leitura para segurança não dá permissão de alteração
+        public LivroRepository(ChapterContext context)  //método construtor para injeção de dependencia
+        {
+            _context = context; // variavel " _context " tem encapsulamento e um nível de segurança.
+        }
+
+        public List<Livro> Listar()
+        {
+            return _context.Livros.ToList();
+        }
+    }
+}
