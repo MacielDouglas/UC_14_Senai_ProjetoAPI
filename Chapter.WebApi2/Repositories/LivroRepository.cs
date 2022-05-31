e8e0f862418781953f2256beb69d1c72edcb82b1
MacielDouglas <douglasmaciel.dm@gmail.com>
2022-05-31 09:45:08 -0300

    Prints da Aol2 e api

---
+++ Chapter.WebApi2/Repositories/LivroRepository.cs
@@ -0,0 +1,20 @@
+﻿using Chapter.WebApi2.Contexts;
+using Chapter.WebApi2.Models;
+
+namespace Chapter.WebApi2.Repositories
+{
+    public class LivroRepository
+    {
+        private readonly ChapterContext _context;
+
+        public LivroRepository(ChapterContext context)
+        {
+            _context = context;
+        }
+
+        public List<Livro> Listar()
+        {
+            return _context.Livros.ToList();
+        }
+    }
+}
